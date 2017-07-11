7400c11995d626dc2913a2b217c163c7c28ebbc6
Robert-Jung <rjinjung@gmail.com>
2017-07-10 22:09:58 -0700

    Adding addArticle method to create a new Article instance and add it to Articles arry.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,12 @@ export class AppComponent {
     ];
   }
 
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${title.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
 }
