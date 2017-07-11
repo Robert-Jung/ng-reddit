0be98641844d746c5a53e97e7f8ed3127e196606
Robert-Jung <rjinjung@gmail.com>
2017-07-10 22:18:08 -0700

    Displaying domain name for each URL.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,9 @@ export class AppComponent {
   
   constructor() {
     this.articles = [
-      new Article('Angular 2', 'http://angular,io', 3),
-      new Article('Fullstack', 'http://fullstack,io', 2),
-      new Article('Angular Homepage', 'http://angular,io', 1),
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
     ];
   }
 
